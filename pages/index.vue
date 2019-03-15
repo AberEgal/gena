4ae34e003b9d494fc294571a1ba74123a9473aa3
AberEgal <stefan@kiecker.info>
2019-03-15 14:43:03 +0100

    img optimized

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,43 @@
 <template>
   <section>
-    <img class="w-full" src="@/static/images/nachhaltigkeit_in_der_natur.jpg" alt="Nachhaltigkeit in der Natur">
-    <h1 class="py-4">Herzlich Willkommen auf generation-nachhaltigkeit.com</h1>
-    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-    <h2 class="pt-4 pb-2">Zwischenüberschrift 1</h2>
+    <div class="h-v50 overflow-hidden">
+      <img src="@/static/images/nachhaltigkeit_in_der_natur.jpg" alt="Nachhaltigkeit in der Natur">
+    </div>
+    <h1
+      class="py-4"
+    >
+      Herzlich Willkommen auf generation-nachhaltigkeit.com
+    </h1>
+    <p>Generation Nachhaltigkeit! Zwei Begriffe, die in unserer heutigen Gesellschaft immer mehr an Bedeutung gewinnen. Denken wir an Greta Thunberg aus Schweden, die weltweit Schüler*innen zu Demonstrationen bewegt, oder Influencer*innen, die auf das Thema „bewusst leben“ setzen. Wir sind ein junges Team und wollen mit dieser Webseite Tipps & Tricks im Umgang mit der Umwelt vorstellen. Konkret: Was kann ich für kleine Veränderungen in meinem Leben vornehmen, um Schritt für Schritt nachhaltiger zu Leben? Jeder einzelne mag vielleicht ein heißer Tropfen auf dem Stein sein, aber zusammen kann es etwas Großes werden z. B. Umweltschutz.</p>
+    <h2
+      class="pt-4 pb-2"
+    >
+      Zwischenüberschrift 1
+    </h2>
     <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </p>
-    <h2 class="pt-4 pb-2">Zwischenüberschrift 2</h2>
+    <h2
+      class="pt-4 pb-2"
+    >
+      Zwischenüberschrift 2
+    </h2>
     <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. </p>
-    <h2 class="pt-4 pb-2">Zwischenüberschrift 3</h2>
+    <h2
+      class="pt-4 pb-2"
+    >
+      Zwischenüberschrift 3
+    </h2>
     <p>Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. </p>
-    <h2 class="pt-4 pb-2">Zwischenüberschrift 4</h2>
+    <h2
+      class="pt-4 pb-2"
+    >
+      Zwischenüberschrift 4
+    </h2>
     <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis. </p>
-    <h2 class="pt-4 pb-2">Zwischenüberschrift 5</h2>
+    <h2
+      class="pt-4 pb-2"
+    >
+      Zwischenüberschrift 5
+    </h2>
     <p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, At accusam aliquyam diam diam dolore dolores duo eirmod eos erat, et nonumy sed tempor et et invidunt justo labore Stet clita ea et gubergren, kasd magna no rebum. sanctus sea sed takimata ut vero voluptua. est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. </p>
   </section>
 </template>
--- tailwind.js
+++ tailwind.js
@@ -517,6 +517,7 @@ module.exports = {
     '32': '8rem',
     '48': '12rem',
     '64': '16rem',
+    'v50': '50vh',
     'full': '100%',
     'screen': '100vh'
   },
